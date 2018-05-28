282669e5dbca163386604d1eab9cc806cc4154b8
RalphWiley <nazkeith@gmail.com>
2018-05-28 13:07:06 -0500

    more changes

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -1,7 +1,7 @@
 var scrape = require("../scripts/scrape");
-var makeDate = require("../scripts/date");
+var makeDate = require("..scripts/date");
 
-var Headline = require("../models/Headline");
+var Headline = require("..models/Headline");
 
 module.exports = {
     fetch: function(cb) {
